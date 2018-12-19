c20041c311d31bd028d77055127166dc04d0ed93
George Spatacean <George.Spatacean@gmail.com>
2018-12-19 16:32:22 +0200

    Split SmartParams classes in separate files

---
+++ src/genapputils/ArgumentsParser.cpp
@@ -0,0 +1,102 @@
+#include "ArgumentsParser.hpp"
+
+template mDLLIMPORTEXPORT std::string ArgumentParser::GetValue<std::string>(const std::string& Name) const;
+template mDLLIMPORTEXPORT bool ArgumentParser::GetValue<bool>(const std::string& Name) const;
+
+template<typename RetVal>
+RetVal ArgumentParser::GetValue(const std::string& Name) const
+{
+    ParamMapConstIter_t elem = mProcessedParams.find(Name);
+    if (elem != mProcessedParams.end())
+        return std::get<RetVal>(elem->second.mValue);
+
+    return {};
+}
+
+ArgumentParser::ArgumentParser(const ParamVector_t& Params)
+    : mAllParameters(Params){};
+
+void ArgumentParser::Parse(int argc, const char* const argv[])
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string str = argv[i];
+        if (str.at(0) == '-') //this is a parameter name
+        {
+            bool isShortName = false;
+            if (str.at(1) != '-') //short name
+            {
+                isShortName = true;
+            }
+
+            //trim leading hyphen(s)
+            str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](char c) {
+                          if (c == '-')
+                              return false;
+                          return true;
+                      }));
+
+            ParamItem theParam;
+            if (isShortName)
+                theParam = FindByName(str, ShortNameComparator);
+            else
+                theParam = FindByName(str, LongNameComparator);
+
+            //now the setting
+            if (theParam.IsBoolean())
+                theParam.mValue = true;
+            else
+                theParam.mValue = std::string(argv[++i]);
+
+            theParam.mIsSet = true;
+
+            //finally push it in the processed map
+            //mProcessedParams[theParam.GetShortName()] = theParam;
+            mProcessedParams.try_emplace(theParam.GetShortName(), theParam);
+        }
+        else
+        {
+            std::cerr << "Unrecognized token: " << str << std::endl;
+        }
+    }
+
+    CheckRequiredParams();
+}
+
+void ArgumentParser::CheckRequiredParams() const
+{
+    for (const ParamItem& item : mAllParameters)
+    {
+        if (item.IsRequired())
+        {
+            const auto& processed = mProcessedParams.find(item.GetShortName());
+            if (processed == mProcessedParams.end() ||
+                !processed->second.IsSet() )
+            {
+                std::ostringstream oss;
+                oss << "Parameter ";
+                oss << item.GetShortName();
+                oss << " is required, but was not given.";
+                throw std::logic_error(oss.str());
+            }
+        }
+
+        // if (item.IsRequired() && !item.IsSet())
+        // {
+        //     std::ostringstream oss;
+        //     oss << "Parameter ";
+        //     oss << item.GetShortName();
+        //     oss << " is required, but was not given.";
+        //     throw std::logic_error(oss.str());
+        // }
+    }
+}
+
+ParamItem ArgumentParser::FindByName(const std::string& ParamName, ParamNameComp_t NameComparator)
+{
+    for (const ParamItem& iter : mAllParameters)
+        if (NameComparator(iter, ParamName))
+            return iter;
+
+    return {};
+}
--- src/genapputils/ParamItem.cpp
+++ src/genapputils/ParamItem.cpp
@@ -1,2 +1,37 @@
 #include "ParamItem.hpp"
 
+ParamItem::ParamItem(
+    const std::string& ParamShortName,
+    bool IsRequiredParam,
+    bool IsBooleanParam,
+    const std::string& LongParamName)
+    : mParamShortName(ParamShortName)
+    , mIsRequired(IsRequiredParam)
+    , mIsBoolean(IsBooleanParam)
+    , mParamLongName(LongParamName)
+{};
+
+std::string ParamItem::GetShortName() const
+{
+    return mParamShortName;
+}
+
+std::string ParamItem::GetLongName() const
+{
+    return mParamLongName;
+}
+
+bool ParamItem::IsBoolean() const
+{
+    return mIsBoolean;
+}
+
+bool ParamItem::IsRequired() const
+{
+    return mIsRequired;
+}
+
+bool ParamItem::IsSet() const
+{
+    return mIsSet;
+}
